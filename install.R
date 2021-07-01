356fae93e5b9b7f6a59a5de6310a248e1d2ca7bd
vtquang194 <79128790+vtquang194@users.noreply.github.com>
2021-07-01 14:15:08 +0900

    Update install.R

--- install.R
+++ install.R
@@ -1,8 +1,3 @@
-install.packages("tidyverse")
-install.packages("rmarkdown")
-install.packages("httr")
-install.packages("shinydashboard")
-install.packages('leaflet')
 install.packages('openair')
 install.packages('rio')
 install.packages('mblm')
